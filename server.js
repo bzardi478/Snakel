1173fa51666538f2ce5f4a5cc21129c1214a3df2
bzardi478 <mzardi07@gmail.com>
2025-04-14 13:35:58 +0100

    Updated server.js with necessary changes 6

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const io = new Server(httpServer, {
  methods: ["GET", "POST"],
  credentials: true
  },
- transports: ['websocket'],
+ transports: ['websocket','polling'],
  pingInterval: 25000,
  pingTimeout: 60000,
  cookie: false,
